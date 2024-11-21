6ca7e09c1e1fd87e3002572e7c7ba5d4b621f8ae
Deep3103 <151928476+Deep3103@users.noreply.github.com>
2024-11-21 16:18:08 +0530

    project_done

---
+++ Project_1/index.js
@@ -0,0 +1,40 @@
+
+const http = require('http');
+const fs = require('fs');
+
+const port = 3103;
+const requrestHandler = (req, res) => {
+  // console.log(req.url);
+  let filePath = "";
+  switch (req.url) {
+    case '/':
+      filePath = './index.html'
+      break;
+    case '/about':
+      filePath = './about.html'
+      break;
+    case '/product':
+      filePath = './product.html'
+      break;
+    case '/contact':
+      filePath = './contact.html'
+      break;
+    default:
+      filePath = './error.html'
+      break;
+  }
+  let data = fs.readFileSync(filePath);
+  res.end(data);
+}
+const server = http.createServer(requrestHandler);     // create a new server
+
+server.listen(port, (err) => {
+  if (err) {
+    console.log(`Server is not start: ${err}`);
+    return false;
+  }
+  console.log(`Server start at http://localhost:${port}`);
+});
+
+
+4
